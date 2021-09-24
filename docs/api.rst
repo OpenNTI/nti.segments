254687461b22ddda414561011781a9924ab6f0ab
Bobby Hagen <bobby.hagen@nextthought.com>
2021-09-24 16:12:15 -0500

    initial commit

---
+++ docs/api.rst
@@ -0,0 +1,13 @@
+===========
+ Reference
+===========
+
+Interfaces
+==========
+
+.. automodule:: nti.segments.interfaces
+
+Model
+=====
+
+.. automodule:: nti.segments.model
---
+++ setup.py
@@ -0,0 +1,89 @@
+import codecs
+from setuptools import setup
+from setuptools import find_packages
+
+entry_points = {
+    'console_scripts': [
+    ],
+}
+
+TESTS_REQUIRE = [
+    'fudge',
+    'nti.testing',
+    'zope.testrunner',
+]
+
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name='nti.segments',
+    version=_read('version.txt').strip(),
+    author='Bobby Hagen',
+    author_email='bobby.hagen@nextthought.com',
+    description="NTI segments",
+    long_description=(
+        _read('README.rst') 
+        + '\n\n' 
+        + _read("CHANGES.rst")
+    ),
+    license='Apache',
+    keywords='segments',
+    classifiers=[
+        'Framework :: Zope3',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    url="https://github.com/NextThought/nti.segments",
+    zip_safe=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    namespace_packages=['nti'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+        'setuptools',
+        'BTrees',
+        'nti.base',
+        'nti.containers',
+        'nti.dublincore',
+        'nti.externalization',
+        'nti.property',
+        'nti.schema',
+        'six',
+        'z3c.schema',
+        'zope.annotation',
+        'zope.cachedescriptors',
+        'zope.catalog',
+        'zope.component',
+        'zope.container',
+        'zope.event',
+        'zope.i18nmessageid',
+        'zope.intid',
+        'zope.interface',
+        'zope.lifecycleevent',
+        'zope.location',
+        'zope.mimetype',
+        'zope.schema',
+        'zope.security',
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+        'docs': [
+            'Sphinx',
+            'repoze.sphinx.autointerface',
+            'sphinx_rtd_theme',
+        ],
+    },
+    entry_points=entry_points,
+)
